479292c39dbacbcffb9c364bd2d96b044bf67313
Alex Cottam <alexcottam12@gmail.com>
2024-11-20 16:09:47 -0700

    add initial files

---
+++ app/recipe/[slug]/page.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import getPostMetadata from '@/utils/getPostMetadata';
+import fs from 'fs';
+import matter from 'gray-matter';
+import Markdown from 'markdown-to-jsx';
+
+function getPostContent(slug) {
+  const folder = 'recipes/';
+  const file = folder + `${slug}.md`;
+  const content = fs.readFileSync(file, 'utf8');
+
+  const matterResult = matter(content);
+  return matterResult;
+}
+
+export const generateStaticParams = async () => {
+  const posts = getPostMetadata('recipes');
+  return posts.map((post) => ({ slug: post.slug }));
+};
+
+export async function generateMetadata({ params, searchParams }) {
+  const id = params?.slug ? ' - ' + params?.slug : '';
+  return {
+    title: `The Bubble Baker ${id.replaceAll('_', ' ')}`,
+  };
+}
+
+const RecipePage = (props) => {
+  const slug = props.params.slug;
+  const post = getPostContent(slug);
+
+  return (
+    <main>
+      <article>
+        <Markdown>{post.content}</Markdown>
+      </article>
+    </main>
+  );
+};
+
+export default RecipePage;
